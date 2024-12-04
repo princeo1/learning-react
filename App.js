c3919445b2b6dd5b9e0c8f0f37dbd7881d9459a1
princeo1 <1996princesoni@gmail.com>
2024-12-04 10:23:41 +0530

    Adding JSX to my code

--- App.js
+++ App.js
@@ -2,38 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [
-        React.createElement(
-            "div",
-            {id: "child1"},
-            [React.createElement("h1",{id: "heading1"},"I am a h1 from child1"),
-            React.createElement("h2",{id: "heading2"},"I am a h2 from child1"),
-            React.createElement("h3",{id: "heading3"},"I am a h3 from child1")]
-        ),
-        React.createElement(
-            "div",
-            {id: "child2"},
-            [React.createElement("h1",{id: "heading"},"I am a h1 from child2"),
-            React.createElement("h2",{id: "heading"},"I am a h2 from child2"),
-            React.createElement("h3",{id: "heading"},"I am a h3 from child2")]
-        )
-    ]
-)
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},
+//     [
+//         React.createElement(
+//             "div",
+//             {id: "child1"},
+//             [React.createElement("h1",{id: "heading1"},"I am a h1 from child1"),
+//             React.createElement("h2",{id: "heading2"},"I am a h2 from child1"),
+//             React.createElement("h3",{id: "heading3"},"I am a h3 from child1")]
+//         ),
+//         React.createElement(
+//             "div",
+//             {id: "child2"},
+//             [React.createElement("h1",{id: "heading"},"I am a h1 from child2"),
+//             React.createElement("h2",{id: "heading"},"I am a h2 from child2"),
+//             React.createElement("h3",{id: "heading"},"I am a h3 from child2")]
+//         )
+//     ]
+// )
 
 
 
 
-const heading = React.createElement(
-    'h1',
-    {
-        id:"heading", // here we give attributes to out element
-        xyz: "abc"    // we can define our own attributes for our element
-    }, 
-    "Hello World from React"
-);
+// const heading = React.createElement(
+//     'h1',
+//     {
+//         id:"heading", // here we give attributes to out element
+//         xyz: "abc"    // we can define our own attributes for our element
+//     }, 
+//     "Hello World from React"
+// );
      // we are creating a heading h1 tag using core of react
+
+ // JSX syntax
+
+const jsxHeading = <h1 id="jsxHeading">I am heading from JSX</h1>
 const root = ReactDOM.createRoot(document.getElementById('root')); // we are creating a root which will be rendered in browsers using React DOM
-root.render(parent); // we are telling react to render the created root
+root.render(jsxHeading); // we are telling react to render the created root
